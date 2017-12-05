1fa4848d2078caff8c049e702dcffe695a5ea895
Karl Hesselgren <karl.hesselgren@freespee.com>
2017-12-05 10:21:19 +0100

    2017 cleanup

--- day10/tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from advent_day10 import Santa
-
-
-class TestCases(unittest.TestCase):
-    test = Santa()
-
-    def test_LookAndSay(self):
-        self.assertEqual("312211", self.test.lookAndSay("1", 5))
